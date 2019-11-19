b70e0cce9f48fb27dac59e3d618935f4626de792
moliv2802 <50334418+moliv2802@users.noreply.github.com>
2019-11-19 21:00:18 +0300

    optimized code, added object pool

--- core/src/com/star/app/game/Asteroid.java
+++ core/src/com/star/app/game/Asteroid.java
@@ -1,9 +1,10 @@
-package com.star.game;
+package com.star.app.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.star.app.screen.ScreenManager;
 
 public class Asteroid {
     private Texture texture;
